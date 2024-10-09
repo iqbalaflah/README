2a173968cd98a427dd05ee1dcf10fcc861c2f516
iqbalaflah <149552543+iqbalaflah@users.noreply.github.com>
2024-10-09 18:16:13 +0700

    Update ASISTENSI P1 DASPROG.c

--- ASISTENSI P1 DASPROG.c
+++ ASISTENSI P1 DASPROG.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    char nama_barang1[50], nama_barang2[50];
-    float harga_barang1, harga_barang2, total;
+    char nama_barang1[50], nama_barang2[50], nama_barang3[50], nama_barang4[50], nama_barang5[50], nama_barang6[50];
+    float harga_barang1, harga_barang2, harga_barang3[50], harga_barang4[50], harga_barang5[50], harga_barang6[50] total;
+    int jumlah_barang1, jumlah_barang2, jumlah_barang3[50], jumlah_barang4[50], jumlah_barang5[50], jumlah_barang6[50];
 
     // Input untuk barang pertama
     printf("Masukkan nama barang pertama: ");
     scanf("%s", nama_barang1);
     printf("Masukkan harga untuk %s: Rp ", nama_barang1);
     scanf("%f", &harga_barang1);
+    printf("Masukkan jumlah untuk %s: ", nama_barang1);
+    scanf("%d", &jumlah_barang1);
 
     // Input untuk barang kedua
     printf("Masukkan nama barang kedua: ");
     scanf("%s", nama_barang2);
     printf("Masukkan harga untuk %s: Rp ", nama_barang2);
     scanf("%f", &harga_barang2);
+    printf("Masukkan jumlah untuk %s: ", nama_barang2);
+    scanf("%d", &jumlah_barang3);
+
+    // Input untuk barang ketiga
+    printf("Masukkan nama barang pertama: ");
+    scanf("%s", nama_barang3);
+    printf("Masukkan harga untuk %s: Rp ", nama_barang3);
+    scanf("%f", &harga_barang3);
+    printf("Masukkan jumlah untuk %s: ", nama_barang3);
+    scanf("%d", &jumlah_barang3);
 
     // Menghitung total
     total = harga_barang1 + harga_barang2;
